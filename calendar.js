ac01037fa5f9c80622661b2a66a8df321ac14d36
Андрей <anderkoval3@gmail.com>
2019-07-19 12:33:10 +0300

    cleaning code and adding comments

--- calendar.js
+++ calendar.js
@@ -1,11 +1,3 @@
-/*
-@todo learn about importing modules.
-In future i will create a separate inline calendar module
-for node-telegram-bot-api
-
-Currently building one
-*/
-
 /**
  * @param {Object} opts object for values
  * @param {String[]} opts.dayNames the sting array of days
@@ -64,13 +56,12 @@ function alignDay(dayToStartWeek, day) {
     }
     return day
 }
-//@todo comments
-// @todo replace undefined with 0
+
 function getMonth(year, month, dayToStartWeek = 0) {
     var weeks = []
     var date = new Date(year, month)
     var first_day = date.getDay()
-    var iter_day = alignDay(dayToStartWeek, first_day)
+    var iter_day = alignDay(dayToStartWeek, first_day) // to alighn calendar to your start week day
     var day_counter = 1
     var week_in_month = true
     while (week_in_month) {
@@ -85,71 +76,14 @@ function getMonth(year, month, dayToStartWeek = 0) {
                 break
             }
         }
-
         iter_day = 0
         if (week_in_month)
             weeks.push(week)
     }
     var month = {
         month: this.opts.monthNames[month], 
-        days: this.opts.dayNames, 
-        weeks_arr: weeks
+        days: this.opts.dayNames, // is not shifted with dayToStartWeek. Watch it yourself 
+        calendar: weeks
     }
     return month
-}
-
-function getMonthMarkup(year, month) {
-    var inline_keyboard = []
-    var days_btns = []
-    var month_btn = [{text: calendar.monthNames[month] + " " + year, callback_data: 0}]
-    inline_keyboard.push(month_btn)
-    calendar.dayNames.forEach(element => {
-        days_btns.push({text: element, callback_data: 0})
-    });
-    inline_keyboard.push(days_btns)
-    var date = new Date(year, month)
-    var day = date.getDay()
-    var array_of_days = new Array(5)
-    for (var i = 0; i < 6; i++) {
-        array_of_days[i] = new Array(7)
-        for (let j = 0; j < 7; j++) {
-            array_of_days[i][j] = {text: ' ', callback_data: 0}
-        }
-    }
-    i = getDayNumFromMonday(day)
-    let day_counter = 1
-    array_of_days[0][i].text = '1'
-    array_of_days.forEach(element => {
-        var week_is_in_month = false
-        for (;i < 7; i++) {
-            date.setDate(day_counter)
-            if (date.getMonth() == month) {
-                week_is_in_month = true
-                element[i].text = (day_counter).toString()
-                element[i].callback_data = new Date(year, month, day_counter).toDateString()
-                day_counter++
-            }
-        }
-
-        i = 0
-        if (week_is_in_month)
-            inline_keyboard.push(element)
-    })
-    var new_right_year = Number(year)
-    var new_left_year = new_right_year
-    var new_right_month = Number(month) + 1
-    if (new_right_month == 12) {
-        new_right_month = 0
-        new_right_year += 1
-    }
-    var new_left_month = (month - 1) < 0 ? 11 : month - 1
-    if (new_left_month == 11) 
-        new_left_year -= 1 
-    var right_btn = {text: ">>>", callback_data: `${new_right_year}_${new_right_month}`}
-    var left_btn =  {text: "<<<", callback_data: `${new_left_year}_${new_left_month}`}
-    inline_keyboard.push([left_btn, right_btn])
-    inline_keyboard.push([{text: 'Назад', callback_data: states.Изменить_дату}])
-    var reply_markup = {}
-    reply_markup.inline_keyboard = inline_keyboard
-    return JSON.stringify(reply_markup)
 }
